1a1676ad4f12a7ed37ff049047aa60526ac65531
lifeibo <lifeibo382005@gmail.com>
2025-03-05 15:24:24 +0800

    support multi platfrom

--- build.sh
+++ build.sh
@@ -1,2 +1,8 @@
-docker build -t lifeibo/ubuntu:25.04 .
+#docker build -t lifeibo/ubuntu:25.04 .
 #docker image push lifeibo/ubuntu:25.04
+
+export DOCKER_CLI_EXPERIMENTAL=enabled
+docker buildx create --use
+
+docker buildx build --platform linux/amd64,linux/arm64 -t lifeibo/ubuntu:25.04 --push .
+
